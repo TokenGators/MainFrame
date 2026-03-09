429d1ab32cd365fe8c7e05183be4bba31d84b6a6
TokenGators <dev@tokengators.com>
2026-03-09 00:46:40 -0700

    chore[gatorrr]: baseline Phaser 2D template structure

---
+++ projects/gatorrr/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    port: 8081,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
